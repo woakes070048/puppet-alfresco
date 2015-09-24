6ef9f3bd45a137001fae4d05976f50b4d1daef45
woakes070048 <woakes070048@gmail.com>
2015-09-24 12:19:31 -0400

    Update aaar.pp

--- manifests/addons/aaar.pp
+++ manifests/addons/aaar.pp
@@ -1,11 +1,13 @@
 class alfresco::addons::aaar inherits alfresco::addons {
 
 	$aaarbase = "https://github.com/fcorti/alfresco-audit-analysis-reporting/releases/download/v3.1"
-
+		    https://github.com/fcorti/alfresco-audit-analysis-reporting/releases/download/v3.1
 case($alfresco_version){
       '4.2.f': {
-          $aaarrepofile = "aaar-repo-v1.1-for-alfresco-CE-v4.2.x.amp"
-          $aaarsharefile = "aaar-share-v1.1-for-alfresco-CE-v4.2.x.amp"
+        #$aaarrepofile = "aaar-repo-v1.1-for-alfresco-CE-v4.2.x.amp"
+        $aaarrepofile = "aaar-repo-v1.1-Alfresco-CE-v4.2.x.amp"
+        #$aaarsharefile = "aaar-share-v1.1-for-alfresco-CE-v4.2.x.amp"
+        $aaarsharefile = "aaar-share-v1.1-Alfresco-CE-v4.2.x.amp"
       }
   		'5.0.x','NIGHTLY': {
 				  $aaarrepofile = "aaar-repo-v1.1-Alfresco-CE-v5.0.d.amp"
